9cd490eb00d6d33cd28feab4e54d9288fd46e37d
ShanMehr <ishanmeher25@gmail.com>
2024-05-22 12:39:11 -0700

    added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(
-    {experimentalReactChildren: true,}
-  )]
+  integrations: [react({
+    experimentalReactChildren: true
+  }), svelte()]
 });
--- src/pages/pdf-reader/PdfReader.tsx
+++ src/pages/pdf-reader/PdfReader.tsx
@@ -44,8 +44,8 @@ const HighlightPopup = ({
     </div>
   ) : null;
 
-const PRIMARY_PDF_URL = "https://arxiv.org/pdf/1708.08021.pdf";
-const SECONDARY_PDF_URL = "https://arxiv.org/pdf/1604.02480.pdf";
+const PRIMARY_PDF_URL = "https://arxiv.org/pdf/1511.08771";
+const SECONDARY_PDF_URL = "https://arxiv.org/pdf/1706.03762";
 
 const searchParams = new URLSearchParams(document.location.search);
 
