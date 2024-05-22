256979333974c5b8d0ed26590214b7ec27f7e4f2
ShanMeher <ishanmeher25@gmail.com>
2024-05-22 00:21:47 -0700

    ininital commit

---
+++ src/pages/react-component/react-component.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import {
+  PdfLoader,
+  PdfHighlighter,
+  Tip,
+  Highlight,
+  Popup,
+  AreaHighlight,
+} from "react-pdf-highlighter";
+
+export const ReactComponent = () => {
+  return (
+    <div>
+      <h1>React Component</h1>
+    </div>
+  );
+}
+
+export default ReactComponent;
